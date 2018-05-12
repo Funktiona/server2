6df4d19ecf5c17d13941d951b58701cf5093adb3
macbookpro <Funktiona@gmail.com>
2018-05-12 21:38:13 +0200

    1

--- main.py
+++ main.py
@@ -5,21 +5,73 @@ import socket
 import time
 
 s = socket.socket()
-host = '192.168.1.239'
-# host = '192.168.0.123' #ip of raspberry pi
+# Host local ip
+#host = '192.168.1.239' OG Network
+#host = '192.168.0.122' # Erik Network
+host = '192.168.0.121'
 port = 10000
 s.bind((host, port))
 
+print(1)
 c = None
 addr = None
 
-s.listen(5)
-while True:
-    c, addr = s.accept()
-    print('after')
-
-
-
-
 app = Flask(__name__, static_url_path='/static')
 socketio = SocketIO(app)
+
+
+@app.route("/index")
+def index():
+
+	return render_template('index.html')
+
+def ret():
+
+	s.listen(5)
+	while True:
+		print('before')
+		c, addr = s.accept()
+		print('after')
+		print('Got connection from', addr)
+
+	return
+
+# Socket is for reciving direction information
+@socketio.on('direction')
+def direction(data):
+
+	print(data)
+
+	while True:
+
+		try:
+
+
+			c.send(data)
+
+			break
+
+		except socket.error, e:
+
+			print(e)
+
+		#c.close()
+		#ret()
+
+	#print('before')
+	#c, addr = s.accept()
+	#print('after')
+	#print('Got connection from', addr)
+
+s.listen(5)
+while True:
+	print('before')
+	c, addr = s.accept()
+	print('after')
+	print('Got connection from', addr)
+
+	break
+
+if __name__ == '__main__':
+    socketio.run(app, host='0.0.0.0', port=8080)
+
